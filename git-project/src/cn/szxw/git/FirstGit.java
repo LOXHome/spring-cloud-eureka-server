116415f95315bc619df3272c8af9ac7edd5caef3
wuzixiao <zixiao.wu@insolu.cn>
2019-04-14 17:32:58 +0800

    调用方法测试

--- git-project/src/cn/szxw/git/FirstGit.java
+++ git-project/src/cn/szxw/git/FirstGit.java
@@ -3,9 +3,11 @@ package cn.szxw.git;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        FirstGit firstGit=new FirstGit();
+        firstGit.gitTest();
     }
 
-    public void gitTest(){
+    public  void gitTest(){
         for (int i=0;i<10;i++){
             System.out.println(i);
         }
